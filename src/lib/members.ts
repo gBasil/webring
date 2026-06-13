5ceecbb3978aaa76a8a8f46ffea90cd54e8dd281
Basil <basil@basil.cafe>
2026-06-13 20:55:55 +0000

    Update member list

--- src/lib/members.ts
+++ src/lib/members.ts
@@ -17,21 +17,22 @@ export enum MemberFlags {
 }
 
 export const members: Member[] = [
-	{ id: 'basil', name: 'Basil', url: 'https://basil.cafe' },
-	{ id: 'aagaming', name: 'AAGaming', url: 'https://aagaming.me' },
-	{ id: 'tasky', name: 'tasky', url: 'https://uwu.network/~tasky' },
+	{ id: 'chloe', name: 'Chloe', url: 'https://sapphic.moe' },
 	{ id: 'alyxia', name: 'Alyxia', url: 'https://alyxia.dev' },
+	{ id: 'basil', name: 'Basil', url: 'https://basil.cafe' },
+	{ id: 'tasky', name: 'tasky', url: 'https://uwu.network/~tasky' },
 	{ id: 'cynthia', name: 'Cynthia', url: 'https://c7.pm' },
 	{ id: 'kyza', name: 'Kyza', url: 'https://github.com/kyza' },
+	{ id: 'aagaming', name: 'AAGaming', url: 'https://aagaming.me' },
 	{ id: 'kneesox', name: 'Kneesox', url: 'https://kneesox.moe' },
 	{ id: 'xirreal', name: 'xirreal', url: 'https://xirreal.dev' },
 	{ id: 'allie', name: 'Allie', url: 'https://cutemoth.uk' },
 	{ id: 'sophia', name: 'sophia', url: 'https://zvava.org' },
-	{ id: 'chloe', name: 'Chloe', url: 'https://sapphic.moe' },
 	{ id: 'overimagine', name: 'OVERIMAGINE', url: 'https://overimagine.xyz' },
 	{ id: 'valerie', name: 'valerie', url: 'https://latte.party' },
 	{ id: 'rort1z2', name: 'R0rt1z2', url: 'https://r0rt1z2.com' },
 	{ id: 'nullptr', name: 'nullptr', url: 'https://github.com/nullpo1nt3d' },
+	{ id: 'maggie', name: 'Maggie', url: 'https://maggiepi.fyi' },
 ];
 
 export const navigatableMembers = members.filter(
